9b23283f6907e9863e391a613ca42c4b6bb6410b
ZJ AND SMY <17854262969@163.com>
2021-11-12 09:15:42 +0800

    test-2

--- gem-dubbo-shop-mybatis/src/main/java/com/atguigu/admin/Boot05WebAdminApplication.java
+++ gem-dubbo-shop-mybatis/src/main/java/com/atguigu/admin/Boot05WebAdminApplication.java
@@ -6,15 +6,28 @@ import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@ResponseBody
 @SpringBootApplication
 @EnableDubbo
 @MapperScan({"com.atguigu.admin.mapper","com.atguigu.admin.boom2.mapper"})
 public class Boot05WebAdminApplication {
 
+
+
+//    @RequestMapping("/hello")
+//    public String hello(){
+//        return "hello,world";
+//    }
+
+
     public static void main(String[] args) {
 
         SpringApplication.run(Boot05WebAdminApplication.class, args);
     }
+
+
 
 }
